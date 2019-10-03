4c027517837de6252d356c3342a25f78fd72b70f
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-02 22:35:26 -0400

    can display user last repos

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ searchUser.addEventListener("keyup", e => {
       } else {
         // show profile
         ui.showProfile(data.profile);
+        // show repos
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- github.js
+++ github.js
@@ -3,6 +3,8 @@ class GitHub {
   constructor() {
     this.client_id = "4754ebc26213efdfe780";
     this.client_secret = "476c000f219928ee126137aa55d81310d21ec043";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   // method to get a github user profile data
@@ -12,10 +14,18 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     // saving a response
     const profile = await profileResponse.json();
+    // saving repos
+    const repos = await repoResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
